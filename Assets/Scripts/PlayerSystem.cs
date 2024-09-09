7e10ad6109739102250841a3f8ff1eaa421899c7
FireLordBulb <79010333+FireLordBulb@users.noreply.github.com>
2024-09-09 13:04:27 +0200

    Made player able to turn

--- Assets/Scripts/PlayerSystem.cs
+++ Assets/Scripts/PlayerSystem.cs
@@ -3,6 +3,7 @@ using Unity.Mathematics;
 using Unity.Transforms;
 
 public partial class PlayerSystem : SystemBase {
+	private static readonly float3 ScreenNormal = new(0, 0, 1);
 	protected override void OnCreate(){
 		RequireForUpdate<PlayerInput>();
 		RequireForUpdate<Player>();
@@ -14,7 +15,9 @@ public partial class PlayerSystem : SystemBase {
 			float3 direction = transform.ValueRO.Up();
 			transform.ValueRW.Position = position + direction*playerMoveInput.Value*(0 < playerMoveInput.Value ? player.MoveSpeed : player.ReverseSpeed)*SystemAPI.Time.DeltaTime;
 			
-			
+			quaternion rotation = transform.ValueRO.Rotation;
+			rotation = math.mul(rotation, quaternion.AxisAngle(ScreenNormal, playerTurnInput.Value*player.TurnSpeed*SystemAPI.Time.DeltaTime));
+			transform.ValueRW.Rotation = rotation;
 		}
 	}
 }
