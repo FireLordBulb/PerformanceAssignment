e2824a8936244c8335f34c67c0898bf644bf05e8
FireLordBulb <79010333+FireLordBulb@users.noreply.github.com>
2024-09-09 11:41:32 +0200

    Made playersystem change the position of the player

--- Assets/Scripts/PlayerSystem.cs
+++ Assets/Scripts/PlayerSystem.cs
@@ -10,8 +10,9 @@ public partial class PlayerSystem : SystemBase {
 	protected override void OnUpdate(){
 		foreach (var (player, playerMoveInput, playerTurnInput, playerShootInput, transform) in SystemAPI.Query<Player, PlayerMoveInput, PlayerTurnInput, PlayerShootInput, RefRW<LocalTransform>>()){
 			float3 position = transform.ValueRO.Position;
-			quaternion rotation = transform.ValueRO.Rotation;
-			transform.ValueRW.Position = position;
+			// ReSharper disable once PossiblyImpureMethodCallOnReadonlyVariable // Not an impure method. 
+			float3 direction = transform.ValueRO.Right();
+			transform.ValueRW.Position = position + direction*player.MoveSpeed*SystemAPI.Time.DeltaTime;
 		}
 	}
 }
