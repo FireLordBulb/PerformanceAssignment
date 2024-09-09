ad71c16e606432f594f0b22d9ba062f6054fb647
FireLordBulb <79010333+FireLordBulb@users.noreply.github.com>
2024-09-09 11:45:43 +0200

    Added prefabs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,4 @@
 using Unity.Entities;
-using UnityEngine;
 
 public struct Player : IComponentData {
 	public Entity ProjectilePrefab;
@@ -7,28 +6,4 @@ public struct Player : IComponentData {
 	public float ReverseSpeed;
 	public float TurnSpeed;
 	public float SecondsPerShot;
-}
-
-public class PlayerAuthoring : MonoBehaviour {
-	public GameObject projectilePrefab;
-	public float moveSpeed; 
-	public float reverseSpeed;
-	public float turnSpeed;
-	public float secondsPerShot;
-
-	public class PlayerBaker : Baker<PlayerAuthoring> {
-		public override void Bake(PlayerAuthoring authoring){
-			Entity entity = GetEntity(TransformUsageFlags.Dynamic);
-			AddComponent(entity, new Player {
-				ProjectilePrefab = GetEntity(authoring.projectilePrefab, TransformUsageFlags.Dynamic),
-				MoveSpeed = authoring.moveSpeed,
-				ReverseSpeed = authoring.reverseSpeed,
-				TurnSpeed = authoring.turnSpeed,
-				SecondsPerShot = authoring.secondsPerShot
-			});
-			AddComponent(entity, new PlayerMoveInput());
-			AddComponent(entity, new PlayerTurnInput());
-			AddComponent(entity, new PlayerShootInput());
-		}
-	}
 }
---
+++ Assets/Scripts/PlayerAuthoring.cs
@@ -0,0 +1,26 @@
+using Unity.Entities;
+using UnityEngine;
+
+public class PlayerAuthoring : MonoBehaviour {
+	public GameObject projectilePrefab;
+	public float moveSpeed; 
+	public float reverseSpeed;
+	public float turnSpeed;
+	public float secondsPerShot;
+
+	public class PlayerBaker : Baker<PlayerAuthoring> {
+		public override void Bake(PlayerAuthoring authoring){
+			Entity entity = GetEntity(TransformUsageFlags.Dynamic);
+			AddComponent(entity, new Player {
+				ProjectilePrefab = GetEntity(authoring.projectilePrefab, TransformUsageFlags.Dynamic),
+				MoveSpeed = authoring.moveSpeed,
+				ReverseSpeed = authoring.reverseSpeed,
+				TurnSpeed = authoring.turnSpeed,
+				SecondsPerShot = authoring.secondsPerShot
+			});
+			AddComponent(entity, new PlayerMoveInput());
+			AddComponent(entity, new PlayerTurnInput());
+			AddComponent(entity, new PlayerShootInput());
+		}
+	}
+}
