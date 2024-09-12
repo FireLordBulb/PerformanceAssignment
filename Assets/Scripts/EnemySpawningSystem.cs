45cabce1994a95382dd206264f7c1182519bfde5
Vilgot Ottestig <vilgot.ottestig@gmail.com>
2024-09-12 10:26:48 +0200

    Made enemies spawn in an arc and be limited by a max enemy count.

--- Assets/Scripts/EnemySpawningSystem.cs
+++ Assets/Scripts/EnemySpawningSystem.cs
@@ -16,21 +16,23 @@ public partial struct EnemySpawningSystem : ISystem {
     public void OnUpdate(ref SystemState state){
         LocalTransform playerTransform = SystemAPI.GetComponent<LocalTransform>(SystemAPI.GetSingletonEntity<PlayerMovement>());
         EntityCommandBuffer commandBuffer = new(Unity.Collections.Allocator.TempJob);
-        foreach (var (spawner, nextWaveTime) in SystemAPI.Query<EnemySpawner, RefRW<NextWaveTime>>()){
+        foreach (var (spawner, nextWaveTime, enemyCount) in SystemAPI.Query<EnemySpawner, RefRW<NextWaveTime>, RefRW<EnemyCount>>()){
             if (SystemAPI.Time.ElapsedTime < nextWaveTime.ValueRO.Value){
                 continue;
             }
             nextWaveTime.ValueRW.Value = spawner.TimePerWave+(float)SystemAPI.Time.ElapsedTime;
-            const float radius = 3.5f;
-            for (int i = 0; i < spawner.EnemiesPerWave; i++){
+            float spawnArcMin = Random.value*math.TAU;
+            float spawnArcMax = spawnArcMin+spawner.SpawnArcAngle;
+            for (int i = 0; i < spawner.EnemiesPerWave && enemyCount.ValueRO.Value < spawner.MaxEnemies; i++){
                 LocalTransform enemyTransform = playerTransform;
-                float randomAngle = Random.value*math.TAU;
-                enemyTransform.Position += radius * new float3(math.cos(randomAngle), math.sin(randomAngle), 0);
+                float spawnAngle = Random.Range(spawnArcMin, spawnArcMax);
+                enemyTransform.Position += spawner.SpawnRadius * new float3(math.cos(spawnAngle), math.sin(spawnAngle), 0);
                 // The enemy sprite's "forward" when having 0 rotation is up (y+ direction), but sin and cos with 0 rotation
                 // is to the right (x+ direction), adding a right angle to the angle will make the enemy look toward the player.
-                enemyTransform.Rotation = quaternion.AxisAngle(GlobalVariables.ScreenNormal, randomAngle+RightAngle);
+                enemyTransform.Rotation = quaternion.AxisAngle(GlobalVariables.ScreenNormal, spawnAngle+RightAngle);
                 Entity newEnemy = commandBuffer.Instantiate(spawner.EnemyPrefab);
                 commandBuffer.SetComponent(newEnemy, enemyTransform);
+                enemyCount.ValueRW.Value++;
             }
         }
         commandBuffer.Playback(state.EntityManager);
