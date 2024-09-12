5ab50a0f39f002d23a500175db087e4c340c405d
Vilgot Ottestig <vilgot.ottestig@gmail.com>
2024-09-12 10:20:07 +0200

    Added more values to enemySpawner

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -3,9 +3,16 @@ using Unity.Entities;
 public struct EnemySpawner : IComponentData {
     public Entity EnemyPrefab;
     public float TimePerWave;
+    public float SpawnRadius;
+    public float SpawnArcAngle;
     public int EnemiesPerWave;
+    public int MaxEnemies;
 }
 
 public struct NextWaveTime : IComponentData {
     public float Value;
+}
+
+public struct EnemyCount : IComponentData {
+    public int Value;
 }
--- Assets/Scripts/EnemySpawnerAuthoring.cs
+++ Assets/Scripts/EnemySpawnerAuthoring.cs
@@ -1,10 +1,14 @@
 using Unity.Entities;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class EnemySpawnerAuthoring : MonoBehaviour {
     public GameObject enemyPrefab;
     public float timePerWave;
+    public float spawnRadius;
+    public float spawnArcAngle;
     public int enemiesPerWave;
+    public int maxEnemies;
 
     public class EnemySpawnerBaker : Baker<EnemySpawnerAuthoring> {
         public override void Bake(EnemySpawnerAuthoring authoring){
@@ -12,8 +16,12 @@ public class EnemySpawnerAuthoring : MonoBehaviour {
             AddComponent(entity, new EnemySpawner{
                 EnemyPrefab = GetEntity(authoring.enemyPrefab, TransformUsageFlags.Dynamic),
                 TimePerWave = authoring.timePerWave,
-                EnemiesPerWave = authoring.enemiesPerWave
+                SpawnRadius = authoring.spawnRadius,
+                SpawnArcAngle = authoring.spawnArcAngle,
+                EnemiesPerWave = authoring.enemiesPerWave,
+                MaxEnemies = authoring.maxEnemies
             });
+            AddComponent(entity, new EnemyCount());
             AddComponent(entity, new NextWaveTime());
         }
     }
