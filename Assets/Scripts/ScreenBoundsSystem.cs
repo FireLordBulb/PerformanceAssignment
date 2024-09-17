d98a57933e226b9b2b3422ae14cbe2c4c8773228
Vilgot Ottestig <vilgot.ottestig@gmail.com>
2024-09-17 15:04:07 +0200

    Fixed screenbounds not working on builds

--- Assets/Scripts/ScreenBoundsSystem.cs
+++ Assets/Scripts/ScreenBoundsSystem.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public partial class ScreenBoundsSystem : SystemBase {
     protected override void OnStartRunning(){
         // If there's no main camera in this game with Asteroids-style screen looping, all hope is lost.
-        Trace.Assert(Camera.main != null);
+        Trace.Assert(Camera.main != null, "CRITICAL ERROR: Can't find main camera!");
+        EntityManager.AddComponent<ScreenBounds>(EntityManager.CreateEntity());
         SystemAPI.SetSingleton(new ScreenBounds(Camera.main));
     }
     protected override void OnUpdate(){}
